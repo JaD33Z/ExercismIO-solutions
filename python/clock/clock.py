45114d8d89d69a61fa92dd3fd664ac5e0a7e64ed
jonathandavis <jondavis8128@gmail.com>
2021-06-18 19:34:17 -0400

    clock

---
+++ python/clock/clock.py
@@ -0,0 +1,33 @@
+class Clock:
+    def __init__(self, hour, minute):
+        sixty_mins, self.minute = divmod(minute, 60)
+        self.hour = (hour + sixty_mins) % 24
+
+    def __repr__(self):
+        return f"{self.hour:02d}:{self.minute:02d}"
+
+    def __eq__(self, other):
+        return repr(self) == other
+
+    def __add__(self, minutes):
+        return Clock(self.hour, self.minute + minutes)
+
+    def __sub__(self, minutes):
+        return Clock(self.hour, self.minute - minutes)
+
+
+
+
+
+hours, minutes = divmod(minutes, 60)
+# print(f'{h:d}:{m:02d}:{s:02d}')
+
+
+
+
+
+
+
+
+
+
